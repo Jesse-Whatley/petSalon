7f4083eb36cc89f3d270b7eefd35729652bfd9f2
Jesse-Whatley <crackerculturecollections@gmail.com>
2024-03-12 23:12:14 -0400

    got most of elements working

--- scripts/display.js
+++ scripts/display.js
@@ -1,6 +1,78 @@
+let text;
+let list;
+let card;
+let row;
+
 function displayNames(){
-    //use for loop to travel the array
+    list = document.getElementById("petList");
+    text="";//clear the variable
+    //use a for loop to travel the array
     for(let i=0; i<petSalon.pets.length; i++){
-        document.getElementById("petList").innerHTML+=`<li>${petSalon.pets[i].name}</li>`;
+        text+=`<li>${petSalon.pets[i].name}</li>`;
+    }
+    list.innerHTML = text;
+}
+
+function displayCards(){
+    const petsDiv = document.getElementById('pets');
+    card = "";
+    for(let i=0; i<petSalon.pets.length; i++){
+        let pet = petSalon.pets[i];
+        card+=`
+        <div class="petCard ${pet.service.toLowerCase()}">
+            <p>${pet.name}</p>
+            <p>${pet.age}</p>
+            <p>${pet.gender}</p>
+            <p>${pet.breed}</p>
+            <p>${pet.service}</p>
+            <p>${pet.type}</p>
+            </div>
+            `;
+    }
+    petsDiv.innerHTML=card;
+}
+
+function displayRows(){
+    const TABLE = document.getElementById('petTable');
+    row = "";
+    for(let i=0; i<petSalon.pets.length; i++){
+        let pet = petSalon.pets[i];
+        row+=`
+        <tr id="${i}">
+            <td>${pet.name}</td>
+            <td>${pet.age}</td>
+            <td>${pet.gender}<td>
+            <td>${pet.breed}<td>
+            <td>${pet.service}</td>
+            <td>${pet.type}</td>
+            <td><button class="btn btn-danger" onclick="deletePet(${i});">🚫</button></td>
+        </tr>    
+            `
+    }
+    TABLE.innerHTML=row;
+}
+
+function displayTotalAmountPets(){
+    document.getElementById('totalPets').innerHTML= petSalon.pets.length;
+
+}
+
+
+function displayServiceCount(){
+    let grooming = 0;
+    let vaccine = 0;
+    let hotel = 0;
+    for(let i=0; i<petSalon.pets.length; i++){
+        let pet = petSalon.pets[i];
+        if(pet.service.toLowerCase() == "grooming"){
+            grooming++;
+            document.getElementById("totalGrooming").innerHTML=grooming;
+        }else if(pet.service.toLowerCase() == "vaccine"){
+            vaccine++;
+            document.getElementById("totalVaccine").innerHTML=vaccine;
+        }else{
+            hotel++;
+            document.getElementById("totalHotel").innerHTML=hotel;
+        }
     }
 }
--- scripts/register.js
+++ scripts/register.js
@@ -1,47 +1,34 @@
 let petSalon = {
-    name:"",
-    phone:""
+    name:"The pet Salon",
+    phone:"999-999-9999",
     address:{
-        country:
-        city:
-        zip:
-    }
+        country:"United States",
+        city:"LaBelle",
+        zip: 12345
+    },
 
-    pets:[
-        {
-            name:"Mesha",
-            age: "1",
-            gender:"female"
-        },
-        {
-            name:"jasmine",
-            age: "15",
-            gender:"female"
-        },
-        {
-            name:"freddy",
-            age: "5",
-            gender:"male"
-        },
-        {
-            name:"shrimp fried rice",
-            age: "2",
-            gender:"male"
-        }
-    ]
+    pets:[]
 }
+let inputName;
+let inputAge;
+let inputGender;
+let inputBreed;
+let inputService;
+let inputType;
 
 function displayPetCount(){
     //hint: length
 }
 
-function displayNames(){
-    //use for loop to travel the array
-    for(let i=0; i<petSalon.pets.length; i++){
-        document.getElementById("petList").innerHTML+=`<li>${petSalon.pets[i].name}</li>`;
-    }
+//creating the constructor
+function Pet(name, age, gender, breed, service, type){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.breed = breed;
+    this.service = service;
+    this.type = type;
 }
-displayNames();
 
 function calculateAverage() {
     let total= 0;
@@ -50,3 +37,81 @@ function calculateAverage() {
     }
     document.getElementById("average").innerHTML+=`<p>${total}</p>`;
 }
+
+function register(){
+    //get the values from the HTML
+    console.log(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value,inputType.value);
+    //create the object using the constructor
+    let newPet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value,inputType.value);
+    //push the object to the array'
+
+    //display the array on the console
+    if(isValid(newPet)){
+        petSalon.pets.push(newPet);
+        displayNames();
+        displayCards();
+        //displayRows();
+        clearForm();
+        displayTotalAmountPets();
+        displayServiceCount();
+        deletePet();
+    }
+}
+
+function isValid(pet){
+    inputName.classList.remove("alert-error");
+    inputService.classList.remove("alert-error");
+    if(!pet.name || !pet.service){
+        if(!pet.name) inputName.classList.add("alert-error");
+        if(!pet.service) inputService.classList.add("alert-error");
+        return false;
+    }
+    return true;
+}
+
+function clearForm(){
+    inputName.value=""; 
+    inputAge.value=""; 
+    inputGender.value=""; 
+    inputBreed.value=""; 
+    inputService.value=""; 
+    inputType.value=""; 
+}
+
+function deletePet(deleteIndex){
+    petSalon.pets.splice(deleteIndex,1);
+    document.getElementById(deleteIndex).remove();
+    displayRows();
+}
+
+function init(){
+
+    //hook
+
+    //initial functions
+    
+
+    //creating a new pet
+    let pet1 = new Pet("Corkey", 1.5, "Male","Doberman","Grooming");
+    let pet2 = new Pet("Chicken Fried Rice", 3, "Female","Bobcat","Vaccine");
+    let pet3 = new Pet("Blue", 1, "Male","Betta Fish","Hotel");
+    petSalon.pets.push(pet1,pet2,pet3);
+    displayNames();
+    displayCards();
+    //displayRows();
+    displayTotalAmountPets();
+    displayServiceCount();
+
+    //load the input
+    inputName = document.getElementById("txtName");
+    inputAge = document.getElementById("txtAge");
+    inputGender = document.getElementById("txtGender");
+    inputBreed = document.getElementById("txtBreed");
+    inputService = document.getElementById("txtService");
+    inputType = document.getElementById("txtType");
+
+    //clear form
+    
+}
+
+window.onload=init;
